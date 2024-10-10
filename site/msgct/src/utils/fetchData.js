2c6a1b7df37c35eaa4484219ed181ea561678b9f
Bradley Johnson <90875992+bjohnson66@users.noreply.github.com>
2024-10-10 16:22:04 -0500

    updated almanac data

--- site/msgct/src/utils/fetchData.js
+++ site/msgct/src/utils/fetchData.js
@@ -3,7 +3,7 @@
 //-------------------------------------
 export const fetchAlmanacData = async () => {
     try {
-      const response = await fetch('/sv_data/gps_data/gps_20241006_204720.json');
+      const response = await fetch('/sv_data/gps_20241010_155756.json');
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
